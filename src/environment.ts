ef015327932b5330b2d3915f677cb3dad70837ba
Walter Banda <walterbanda98@gmail.com>
2023-05-12 09:40:35 +0200

    commit

--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  serverUrl: 'https://ae93-197-211-216-189.ngrok-free.app/',
+  serverUrl: 'https://bc36-197-211-216-189.ngrok-free.app/',
   // serverUrl: 'http://localhost:3000/',
 };
